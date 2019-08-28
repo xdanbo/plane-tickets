a3a6267238e64a961e4025fe5659c21798dab041
Artem Prymostka <artemp.mail@gmail.com>
2019-08-28 08:04:33 +0300

    Update error-indicator.jsx

--- src/components/error-indicator/error-indicator.jsx
+++ src/components/error-indicator/error-indicator.jsx
@@ -5,7 +5,7 @@ import './error-indicator.scss';
 const ErrorIndicator = () => {
 	return (
 		<div className="content-block error-mod">
-			Oops, we've got an error!
+			Ой... Что-то пошло не так :(
 		</div>
 	);
 };
